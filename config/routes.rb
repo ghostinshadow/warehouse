0c670dc7830f75d12c5076531423cbbfb2d7ccdb
Oleksandr Kychun <kichun.oleksandr@gmail.com>
2017-03-15 23:45:25 +0200

    sign in/out tests; units(read create)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   devise_for :users
+  resources :units
   root 'welcome#index'
 end
--- spec/features/users/sign_out_spec.rb
+++ spec/features/users/sign_out_spec.rb
@@ -1,19 +1,12 @@
-# Feature: Sign out
-#   As a user
-#   I want to sign out
-#   So I can protect my account from unauthorized access
 feature 'Sign out', :devise do
 
   # Scenario: User signs out successfully
-  #   Given I am signed in
-  #   When I sign out
-  #   Then I see a signed out message
   scenario 'user signs out successfully' do
     user = FactoryGirl.create(:user)
     signin(user.email, user.password)
-    expect(page).to have_content I18n.t 'devise.sessions.signed_in'
+    expect(page).to have_content("Signed in successfully")
     click_link 'Sign out'
-    expect(page).to have_content I18n.t 'devise.sessions.signed_out'
+    expect(page).to have_content("You need to sign in or sign up before continuing")
   end
 
 end
