74f0cc6eeddebebe0d558e575394e9514382406c
Oleksandr Kychun <kichun.oleksandr@gmail.com>
2017-04-18 19:05:45 +0300

    small fix for resource type display

--- app/models/countable_resource.rb
+++ app/models/countable_resource.rb
@@ -6,4 +6,8 @@ class CountableResource < Resource
   def >>(num)
     self.count -= BigDecimal(num)
   end
+
+  def to_s
+    I18n.t('resources.countable')
+  end
 end
--- app/models/countless_resource.rb
+++ app/models/countless_resource.rb
@@ -2,4 +2,8 @@ class CountlessResource < Resource
   def <<(num); end
 
   def >>(num); end
+
+  def to_s
+    I18n.t('resources.countless')
+  end
 end
